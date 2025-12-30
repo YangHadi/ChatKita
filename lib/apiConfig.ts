8461a7a4bcd9941fd9d26f5bb99a833ef3f3fa93
YangHadi <yanghadi900@gmail.com>
2025-12-30 13:36:04 +0800

    rename backend folder to chatkita-backend

--- lib/apiConfig.ts
+++ lib/apiConfig.ts
@@ -11,5 +11,5 @@ const PC_LOCAL_IP = "192.168.88.212";
 // The base URL for your backend API.
 // It uses the local IP for Android development and 'localhost' for others (like web).
 export const API_BASE_URL = Platform.OS === 'android'
-  ? `http://${PC_LOCAL_IP}/simplechat-backend`
-  : `http://localhost/simplechat-backend`;
+  ? `http://${PC_LOCAL_IP}/chatkita-backend`
+  : `http://localhost/chatkita-backend`;
